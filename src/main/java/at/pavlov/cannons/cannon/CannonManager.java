379eb77ca0d82b3365997d82d81234d45eade089
galacticwarrior9 <zareef1@yahoo.com>
2022-12-17 00:41:30 +0000

    Wrap nullable cannon in optional to allow caching
    Guava's CacheBuilder does not accept null values

--- src/main/java/at/pavlov/cannons/cannon/CannonManager.java
+++ src/main/java/at/pavlov/cannons/cannon/CannonManager.java
@@ -30,13 +30,14 @@ import at.pavlov.cannons.config.UserMessages;
 import at.pavlov.cannons.container.SimpleBlock;
 import at.pavlov.cannons.event.CannonAfterCreateEvent;
 import at.pavlov.cannons.event.CannonBeforeCreateEvent;
+import org.jetbrains.annotations.Nullable;
 
 
 public class CannonManager
 {
     private static final ConcurrentHashMap<UUID, Cannon> cannonList = new ConcurrentHashMap<UUID, Cannon>();
     private static final ConcurrentHashMap<String, UUID> cannonNameMap = new ConcurrentHashMap<String, UUID>();
-    private static final Cache<Location, Cannon> cannonCache = CacheBuilder.newBuilder()
+    private static final Cache<Location, Optional<Cannon>> cannonCache = CacheBuilder.newBuilder()
             .expireAfterAccess(60, TimeUnit.SECONDS)
             .build();
 
@@ -466,9 +467,10 @@ public class CannonManager
         return null;
     }
 
-    private Cannon getCannonFromLocation(Location location) {
+    private @Nullable Cannon getCannonFromLocation(Location location) {
         try {
-            return cannonCache.get(location, () -> getCannonFromStorage(location));
+            Optional<Cannon> optionalCannon = cannonCache.get(location, () -> Optional.ofNullable(getCannonFromStorage(location)));
+            return optionalCannon.orElse(null);
         }
         // Throws if there was no cannon at the location (i.e. the cannon is null)
         catch (CacheLoader.InvalidCacheLoadException ex) {
